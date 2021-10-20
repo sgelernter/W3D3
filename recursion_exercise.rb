934b097ece59b0caf7f0eb9eda857172a036e1ee
nbarr <nbarrameda89@gmail.com>
2021-10-20 14:19:11 -0400

    dup done

--- recursion_exercise.rb
+++ recursion_exercise.rb
@@ -36,3 +36,42 @@ end
 # p exp_2(2, 3)
 # p exp_2(3, 3)
 # p exp_2(10, 0)
+
+
+def deep_dup(arr)
+    return arr.dup if arr.none? {|ele| ele.is_a?(Array)}
+    
+    # remainder = deep_dup(arr[1..-1])
+
+    arr.map do |ele|
+        if ele.is_a?(Array) && ele.any? {|subele| subele.is_a?(Array)}
+            deep_dup(ele)
+        elsif !ele.is_a?(Array)
+            ele
+        else
+            ele.dup
+        end
+    end
+
+end
+
+            
+
+    # if arr.first.is_a?(Array)
+    #     deep_dup(arr[0]) + deep_dup(remainder)
+    # else
+    #     arr[0].dup + deep_dup(remainder)
+    # end
+
+end
+
+robot_parts = [
+    ["nuts", "bolts", "washers"],
+    ["capacitors", "resistors", "inductors"]
+]
+
+robot_parts_copy = deep_dup(robot_parts)
+# arr1 = [1, [2], [3, [4]]]
+# arr2 = deep_dup([1, [2], [3, [4]]])
+# sam = [1, [2, 3]]
+# sam2 = deep_dup(sam)
