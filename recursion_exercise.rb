22ae9b47fba5611ae3f1e939c8c569901999ddf0
nbarr <nbarrameda89@gmail.com>
2021-10-20 17:11:04 -0400

    bsearch and merge sort

--- recursion_exercise.rb
+++ recursion_exercise.rb
@@ -68,3 +68,74 @@ def rec_fib(n)
     fib_array << fib_array[-2..-1].sum
 end
 
+def bsearch(array, target)
+    middle = array.length / 2
+    check_element = array[middle]   
+    # return nil if !array.include?(target)
+    return nil if array.first != target && array.length == 1
+    return middle if check_element == target
+    if check_element > target
+        bsearch(array[0...middle], target) 
+    else
+        begin
+            array[0..middle].length + bsearch(array[middle + 1..-1], target)
+        rescue
+            return nil
+        end
+    end
+end
+
+# p bsearch([1, 2, 3], 1) # => 0
+# p bsearch([2, 3, 4, 5], 3) # => 1
+# p bsearch([2, 4, 6, 8, 10], 6) # => 2
+# p bsearch([1, 3, 4, 5, 9], 5) # => 3
+# p bsearch([1, 2, 3, 4, 5, 6], 6) # => 5
+# p bsearch([1, 2, 3, 4, 5, 6], 0) # => nil
+# p bsearch([1, 2, 3, 4, 5, 7], 6) # => nil
+
+def merge_sort(array)
+    return array if array.length == 0 || array.length == 1
+
+    middle = array.length / 2
+
+    # debugger
+    merge(merge_sort(array[0...middle]), merge_sort(array[middle..-1]))
+
+end
+
+def merge(array1, array2)
+    merge = []
+
+    if array1.length <= array2.length
+        shorter = array1
+        longer = array2
+    else
+        shorter = array2
+        longer = array1
+    end
+
+    (0...shorter.length).each do |i|
+        if array1[i] <= array2[i]
+            merge.push(array1[i], array2[i])
+        else
+            merge.push(array2[i], array1[i])
+        end
+    end 
+
+    if array1.length != array2.length
+        merge.push(longer.last)
+        if merge[-1] < merge[-2]
+            merge[-1], merge[-2] = merge[-2], merge[-1]
+        end
+        merge
+    else
+        merge
+    end
+
+end
+
+# p merge([9], [1, 3])
+# p merge_sort([4, 3, 1])
+p merge_sort([4, 2, 3, 1, 8])
+# p merge_sort([4, 2, 3, 1, 7, 9, 2, 5, 8, 5, 7, 2])
+# p merge_sort([4, 2, 3, 1, 7, 9, 2, 5, 8, 5, 7, 2, 9, 3, 4, 8])
